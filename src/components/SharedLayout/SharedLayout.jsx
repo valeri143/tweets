f48defa139a25e91234b50a378ecfd1e275049ae
valeri143 <valeria23200523251605@gmail.com>
2023-10-11 15:39:03 +0300

    added loader

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,27 @@
 import { Suspense } from 'react';
+import { BallTriangle } from 'react-loader-spinner';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
+import { StyledLoaderWrapper } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <StyledLoaderWrapper>
+            <BallTriangle
+              height={200}
+              width={200}
+              radius={5}
+              color="#9803fc"
+              ariaLabel="ball-triangle-loading"
+              visible={true}
+            />
+          </StyledLoaderWrapper>
+        }
+      >
         <Outlet />
       </Suspense>
     </>
