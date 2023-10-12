36c56a3989c960a8654d47f101a67a292d74c10a
valeri143 <valeria23200523251605@gmail.com>
2023-10-12 22:41:11 +0300

    .

--- src/pages/TweetsPage/TweetsPage.jsx
+++ src/pages/TweetsPage/TweetsPage.jsx
@@ -15,6 +15,7 @@ import { StyledTweetButton } from '../../components/TweetCard/TweetCard.styled';
 
 const TweetsPage = () => {
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const [page, setPage] = useState(1);
   const [isVisibleButton, setIsVisibleButton] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -41,20 +42,33 @@ const TweetsPage = () => {
     setPage((prevPage) => prevPage + 1);
   };
 
+  const followingUsers = users.filter((user) => {
+    const isFollowing = localStorage.getItem(`isFollowing_${user.id}`);
+    return isFollowing === 'true';
+  });
+
+  const withoutFollowingUsers = users.filter((user) => {
+    const isFollowing = localStorage.getItem(`isFollowing_${user.id}`);
+    return isFollowing === 'false';
+  });
+
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
     setPage(1);
-  };
-  const filteredUsers = users.filter((user) => {
-    if (filter === 'show all') {
-      return true;
-    } else if (filter === 'follow') {
-      return localStorage.getItem(`isFollowing_${user.id}`);
-    } else if (filter === 'followings') {
-      return !localStorage.getItem(`isFollowing_${user.id}`);
+    switch (event.target.value) {
+      case 'show all':
+        return setFilteredUsers(users);
+
+      case 'follow':
+        return setFilteredUsers(withoutFollowingUsers);
+
+      case 'followings':
+        return setFilteredUsers(followingUsers);
+
+      default:
+        break;
     }
-    return false;
-  });
+  };
 
   return (
     <>
@@ -72,18 +86,29 @@ const TweetsPage = () => {
       </StyledDropdown>
       <StyledContainer>
         <StyledTweetsUl>
-          {filteredUsers &&
-            filteredUsers.map((user) => (
-              <li key={user.id}>
-                <TweetCard
-                  cardId={user.id}
-                  tweets={user.tweets}
-                  followers={user.followers}
-                  avatar={user.avatar}
-                  setUsers={setUsers}
-                />
-              </li>
-            ))}
+          {filteredUsers.length !== 0
+            ? filteredUsers.map((user) => (
+                <li key={user.id}>
+                  <TweetCard
+                    cardId={user.id}
+                    tweets={user.tweets}
+                    followers={user.followers}
+                    avatar={user.avatar}
+                    setUsers={setUsers}
+                  />
+                </li>
+              ))
+            : users.map((user) => (
+                <li key={user.id}>
+                  <TweetCard
+                    cardId={user.id}
+                    tweets={user.tweets}
+                    followers={user.followers}
+                    avatar={user.avatar}
+                    setUsers={setUsers}
+                  />
+                </li>
+              ))}
         </StyledTweetsUl>
         {isLoading && <p>Loading...</p>}
         {isVisibleButton && (
