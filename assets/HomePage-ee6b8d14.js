0d0a06770eae2c31e54a6157b3dbb8be0323f99c
valeri143 <117216733+valeri143@users.noreply.github.com>
2023-10-13 15:11:20 +0000

    Deploying to gh-pages from @ valeri143/tweets-app@38e4c37d89581f533019afc5ef761e9f7c18f78d 🚀

---
+++ assets/HomePage-ee6b8d14.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-bd95bf26.js";import{S as t,a as o}from"./HomePage.styled-3f751b51.js";const s=()=>e.jsx(t,{children:e.jsx(o,{children:"Welcome to the Home Page"})});export{s as default};
---
+++ assets/HomePage.styled-3f751b51.js
@@ -0,0 +1,31 @@
+import{n as t}from"./index-bd95bf26.js";const n=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 320px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  @media screen and (min-width: 390px) {
+    width: 390px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+  @media screen and (min-width: 1200px) {
+    width: max-content;
+  }
+`,a=t.h1`
+  text-align: center;
+  margin: 0;
+  font-size: 60px;
+  color: var(--light-color);
+  text-shadow:
+    0 0 5px #00baff,
+    0 0 10px #00baff,
+    0 0 20px #00baff,
+    0 0 40px #00baff,
+    0 0 80px #00baff;
+`;export{n as S,a};
